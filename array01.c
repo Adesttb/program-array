daa0ab95978b8cdd1f4c4308a29b3c92c03a2d1b
Ade komarudin <32735165+Adesttb@users.noreply.github.com>
2018-02-08 16:02:02 +0700

    Add files via upload

---
+++ array01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+    int main()
+{
+    int a[5];
+                    printf("Input 5 buah angka (pisah dengan spasi) : ");
+                    scanf("%d %d %d %d %d",&a[1],&a[2],&a[3],&a[4],&a[5]);
+                    printf("\ntotal = %d",a[1]+a[2]+a[3]+a[4]+a[5]);
+
+                return 0;
+}
